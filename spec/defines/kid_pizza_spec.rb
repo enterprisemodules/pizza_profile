7a404b5173683525e577097b59439b304363c2f4
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-08-04 17:19:45 +0200

    step five - Add the test cases to the RSpec file

--- spec/defines/kid_pizza_spec.rb
+++ spec/defines/kid_pizza_spec.rb
@@ -4,16 +4,70 @@ require 'spec_helper'
 
 describe 'pizza_profile::kid_pizza' do
   let(:title) { 'namevar' }
-  let(:params) do
-    {}
-  end
 
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      # it { is_expected.to compile }
-      it { dump_catalog }
+      context 'kids younger then 2' do
+        let(:params) do
+          { :age => 1 }
+        end
+      end
+
+      context 'kids between 2 and 5' do
+        let(:params) do
+          { :age => 3 }
+        end
+        it { is_expected.to compile }
+      end
+
+      context 'kids between 5 and 8' do
+        let(:params) do
+          { :age => 7 }
+        end
+        it { is_expected.to compile }
+      end
+
+      context 'kids older then 8 (default case)' do
+        let(:params) do
+          {}
+        end
+        it { is_expected.to compile }
+
+        it { is_expected.to contain_class('Settings')
+        }
+        
+        it { is_expected.to contain_pizza_profile__kid_pizza('namevar')
+          .with('age'    => '12')
+          .with('dough'  => 'white')
+          .with('cheese' => 'mozzarella')
+        }
+        
+        it { is_expected.to contain_crust('namevar/medium_wholesome_thin_crust')
+          .with('ensure' => 'baked')
+          .with('size'   => '30')
+          .with('type'   => 'thin')
+          .with('dough'  => 'wholesome')
+          .that_comes_before('Tomato_sauce[namevar/thick_cristal]')
+        }
+        
+        it { is_expected.to contain_tomato_sauce('namevar/thick_cristal')
+          .with('ensure'    => 'present')
+          .with('type'      => 'cristal')
+          .with('composure' => 'thick')
+          .with('amount'    => '6')
+          .that_comes_before('Cheese[namevar/a_lot_of_mozzarella]')
+        }
+        
+        it { is_expected.to contain_cheese('namevar/a_lot_of_mozzarella')
+          .with('ensure' => 'present')
+          .with('type'   => 'mozzarella')
+          .with('amount' => '5')
+        }
+      end
+
+
     end
   end
 end
