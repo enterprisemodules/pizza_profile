2d4f323f0341204c383004258b0a5f059cc0c720
Bert Hajee <bert.hajee@enterprisemodules.com>
2021-08-04 16:30:44 +0200

    step one - generate the empty defined type and tests

---
+++ manifests/kid_pizza.pp
@@ -0,0 +1,9 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   pizza_profile::kid_pizza { 'namevar': }
+define pizza_profile::kid_pizza (
+) {
+}
